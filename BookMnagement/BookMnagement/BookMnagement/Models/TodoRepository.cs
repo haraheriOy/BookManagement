2b699e4b2fbf5d12994f327b95acbd5915a7dab2
okita <oy.perucuma.625@gmail.com>
2019-07-26 15:07:28 +0900

    test

---
+++ BookMnagement/BookMnagement/BookMnagement/Models/TodoRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Xamarin.Forms;
+
+namespace BookMnagement.Models
+{
+    class TodoRepository
+    {
+        static readonly object Locker = new object();
+        readonly SQLiteConnection _db;
+
+        public TodoRepository()
+        {
+            _db = DependencyService.Get<Models.ISQLite>().GetConnection();
+            _db.DropTable<SQLiteItem>();
+            _db.CreateTable<SQLiteItem>();
+            var item = new SQLiteItem();
+            item.UserName = "user";
+            item.Password = "5kaime";
+            _db.Insert(item);
+        }
+
+        public IEnumerable<SQLiteItem> GetItems() { 
+            lock(Locker){
+                // Delete==falseの一覧を取得する（削除フラグが立っているものは対象外）
+                return _db.Table<SQLiteItem>().Where(m => m.Delete == false);
+            }
+        }
+        public int SaveItem(SQLiteItem item)
+        {
+            lock (Locker)
+            {
+                if(item.Id != 0)
+                {
+                    _db.Update(item);
+                    return item.Id;
+                }
+                return _db.Insert(item);
+            }
+        }
+        
+        // sqlの実行
+        public IEnumerable<Models.SQLiteItem> ExeSql(string sql, string userName)
+        {
+            return _db.Query<Models.SQLiteItem>(sql,userName);
+        }
+    }
+}
---
+++ BookMnagement/BookMnagement/BookMnagement/Views/LoginPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xamarin.Forms;
+using SQLite;
+using System.Collections.Generic;
+
+namespace BookMnagement.Views
+{
+	public partial class LoginPage : ContentPage
+	{
+        readonly Models.TodoRepository _db = new Models.TodoRepository();
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+		async void OnSignUpButtonClicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new BookMnagement.Views.SignUpPage());
+		}
+
+		async void OnLoginButtonClicked (object sender, EventArgs e)
+		{
+            var user = new BookMnagement.Models.User
+            {
+                Username = usernameEntry.Text,
+                Password = passwordEntry.Text
+            };
+            string sql = "select * from SQLiteItem where UserName = ?";
+            IEnumerable < Models.SQLiteItem > data = _db.ExeSql(sql, user.Username);
+            //data.GetEnumerator();
+            foreach (Models.SQLiteItem userdata in data)
+            {
+                if (userdata.Password.Equals(user.Password))
+                {
+                    App.IsUserLoggedIn = true;
+                    Navigation.InsertPageBefore(new MainPage(), this);
+                    await Navigation.PopAsync();
+                }
+                else
+                {
+                    messageLabel.Text = "not data";
+                    passwordEntry.Text = string.Empty;
+                }
+            }
+            
+            /* 
+            var isValid = AreCredentialsCorrect (user);
+			if (isValid) {
+				App.IsUserLoggedIn = true;
+				Navigation.InsertPageBefore (new MainPage (), this);
+				await Navigation.PopAsync ();
+			} else {
+				messageLabel.Text = "Login failed";
+				passwordEntry.Text = string.Empty;
+			}
+            */
+		}
+
+		bool AreCredentialsCorrect (BookMnagement.Models.User user)
+		{
+			return user.Username == BookMnagement.Models.Constants.Username && user.Password == BookMnagement.Models.Constants.Password;
+		}
+	}
+}
